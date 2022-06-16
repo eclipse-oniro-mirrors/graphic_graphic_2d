clipsheep6/graphic_2d

rebuild ST/UT structure
Signed-off-by: Grady <yangguangwei4@huawei.com>
Change-Id: I18d028f74738c9ffc4d58bd3e99a9dcc03a4f5e6

rosen/modules/render_service_client/test/systemtest/rs_interfaces_test_utils.cpp
@@ -1,0 +1,135 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rs_interfaces_test_utils.h"
+#include "platform/common/rs_log.h"
+
+namespace OHOS {
+namespace Rosen {
+
+RSInterfaceTestUtils::~RSInterfaceTestUtils()
+{
+    if (prevBuffer_ != nullptr) {
+        SurfaceError ret = csurface_->ReleaseBuffer(prevBuffer_, -1);
+        if (ret != SURFACE_ERROR_OK) {
+            ROSEN_LOGE("buffer release failed");
+            return;
+        }
+        ROSEN_LOGI("prevBuffer_ release success");
+    }
+    if (csurface_ != nullptr) {
+        csurface_->UnregisterConsumerListener();
+    }
+    csurface_ = nullptr;
+    psurface_ = nullptr;
+    listener_ = nullptr;
+    prevBuffer_ = nullptr;
+    bufferHandle_ = nullptr;
+}
+
+bool RSInterfaceTestUtils::CreateSurface()
+{
+    csurface_ = Surface::CreateSurfaceAsConsumer();
+    if (csurface_ == nullptr) {
+        ROSEN_LOGE("csurface create failed");
+        return false;
+    }
+
+    auto producer = csurface_->GetProducer();
+    psurface_ = Surface::CreateSurfaceAsProducer(producer);
+    if (psurface_ == nullptr) {
+        ROSEN_LOGE("csurface create failed");
+        return false;
+    }
+
+    listener_ = new BufferListener(*this);
+    SurfaceError ret = csurface_->RegisterConsumerListener(listener_);
+    if (ret != SURFACE_ERROR_OK) {
+        ROSEN_LOGE("listener register failed");
+        return false;
+    }
+    return true;
+}
+
+void RSInterfaceTestUtils::OnVsync()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    ROSEN_LOGI("RSInterfaceTestUtils::OnVsync");
+    sptr<SurfaceBuffer> cbuffer = nullptr;
+    int32_t fence = -1;
+    int64_t timestamp = 0;
+    OHOS::Rect damage;
+    if (csurface_ == nullptr) {
+        ROSEN_LOGE("csurface_ is null");
+        return;
+    }
+    auto sret = csurface_->AcquireBuffer(cbuffer, fence, timestamp, damage);
+    UniqueFd fenceFd(fence);
+    if (cbuffer == nullptr || sret != OHOS::SURFACE_ERROR_OK) {
+        ROSEN_LOGE("acquire buffer failed");
+        return;
+    }
+    bufferHandle_ = cbuffer->GetBufferHandle();
+    if (bufferHandle_ == nullptr) {
+        ROSEN_LOGE("get bufferHandle failed");
+        return;
+    }
+    if (defaultWidth_ == static_cast<uint32_t>(bufferHandle_->width) &&
+        defaultHeight_ == static_cast<uint32_t>(bufferHandle_->height)) {
+        successCount_++;
+        ROSEN_LOGI("compareWH is successful in onVsync: %d", successCount_);
+    } else {
+        failCount_++;
+    }
+    if (cbuffer != prevBuffer_) {
+        if (prevBuffer_ != nullptr) {
+            SurfaceError ret = csurface_->ReleaseBuffer(prevBuffer_, -1);
+            if (ret != SURFACE_ERROR_OK) {
+                ROSEN_LOGE("buffer release failed");
+                return;
+            }
+        }
+        prevBuffer_ = cbuffer;
+    }
+}
+
+void RSInterfaceTestUtils::RootNodeInit(std::shared_ptr<RSUIDirector> rsUiDirector, int width, int height)
+{
+    std::cout << "rs app demo Init Rosen Backend!" << std::endl;
+    rootNode_ = RSRootNode::Create();
+    rootNode_->SetBounds(0, 0, width, height);
+    rootNode_->SetFrame(0, 0, width, height);
+    rootNode_->SetBackgroundColor(SK_ColorRED);
+    rsUiDirector->SetRoot(rootNode_->GetId());
+}
+
+sptr<Window> RSInterfaceTestUtils::CreateWindowByDisplayParam(DisplayId displayId, std::string name,
+    int32_t x, int32_t y, int32_t w, int32_t h)
+{
+    sptr<WindowOption> option = new WindowOption();
+    if (option == nullptr) {
+        return nullptr;
+    }
+    Rect displayRect = {x, y, w, h};
+    option->SetDisplayId(displayId);
+    option->SetWindowRect(displayRect);
+    option->SetWindowType(WindowType::APP_MAIN_WINDOW_BASE);
+    option->SetWindowMode(WindowMode::WINDOW_MODE_FLOATING);
+    option->SetWindowName(name);
+    sptr<Window> window = Window::Create(option->GetWindowName(), option);
+    return window;
+}
+} // namespace ROSEN
+} // namespace OHOS

rosen/modules/render_service_client/test/systemtest/rs_interfaces_test_utils.h
@@ -1,0 +1,80 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FRAMEWORKS_RS_TEST_ST_RS_INTERFACES_TEST_UTILS_H
+#define FRAMEWORKS_RS_TEST_ST_RS_INTERFACES_TEST_UTILS_H
+
+#include <securec.h>
+
+#include "display_manager.h"
+#include "screen_manager.h"
+#include "display_property.h"
+#include "display.h"
+#include "screen.h"
+#include "display_info.h"
+#include "window.h"
+#include "window_option.h"
+#include "wm_common.h"
+#include "dm_common.h"
+#include "window_manager_hilog.h"
+#include "unique_fd.h"
+#include "ui/rs_display_node.h"
+#include "ui/rs_root_node.h"
+#include "ui/rs_surface_node.h"
+#include "ui/rs_ui_director.h"
+
+namespace OHOS {
+namespace Rosen {
+class RSInterfaceTestUtils {
+public:
+    ~RSInterfaceTestUtils();
+    bool CreateSurface();
+    void RootNodeInit(std::shared_ptr<RSUIDirector> rsUiDirector, int width, int height);
+    sptr<Window> CreateWindowByDisplayParam(DisplayId displayId, std::string name,
+        int32_t x, int32_t y, int32_t w, int32_t h);
+    class BufferListener : public IBufferConsumerListener {
+    public:
+        explicit BufferListener(RSInterfaceTestUtils &rsInterfaceTestUtils): utils_(rsInterfaceTestUtils)
+        {
+        }
+        ~BufferListener() noexcept override = default;
+        void OnBufferAvailable() override
+        {
+            utils_.OnVsync();
+        }
+
+    private:
+        RSInterfaceTestUtils &utils_;
+    };
+    friend class BufferListener;
+    
+    void OnVsync();
+    uint32_t successCount_ = 0;
+    uint32_t failCount_ = 0;
+    uint32_t defaultWidth_ = 0;
+    uint32_t defaultHeight_ = 0;
+    sptr<IBufferConsumerListener> listener_ = nullptr;
+    sptr<Surface> csurface_ = nullptr; // cosumer surface
+    sptr<Surface> psurface_ = nullptr; // producer surface
+    sptr<SurfaceBuffer> prevBuffer_ = nullptr;
+    BufferHandle *bufferHandle_ = nullptr;
+    std::shared_ptr<RSNode> rootNode_ = nullptr;
+
+private:
+    std::mutex mutex_;
+};
+} // namespace ROSEN
+} // namespace OHOS
+#endif // FRAMEWORKS_RS_TEST_ST_RS_INTERFACES_TEST_UTILS_H

eclipse-oniro-mirrors/graphic_graphic_2d

rosen/modules/render_service_client/test/systemtest/rs_interfaces_test_utils.cpp
@@ -1,0 +1,135 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rs_interfaces_test_utils.h"
+
+#include "platform/common/rs_log.h"
+
+namespace OHOS {
+namespace Rosen {
+RSInterfacesTestUtils::~RSInterfacesTestUtils()
+{
+    if (prevBuffer_ != nullptr) {
+        SurfaceError ret = cSurface_->ReleaseBuffer(prevBuffer_, -1);
+        if (ret != SURFACE_ERROR_OK) {
+            ROSEN_LOGE("buffer release failed");
+            return;
+        }
+        ROSEN_LOGI("prevBuffer_ release success");
+    }
+    if (cSurface_ != nullptr) {
+        cSurface_->UnregisterConsumerListener();
+    }
+    cSurface_ = nullptr;
+    pSurface_ = nullptr;
+    listener_ = nullptr;
+    prevBuffer_ = nullptr;
+    bufferHandle_ = nullptr;
+}
+
+bool RSInterfacesTestUtils::CreateSurface()
+{
+    cSurface_ = Surface::CreateSurfaceAsConsumer();
+    if (cSurface_ == nullptr) {
+        ROSEN_LOGE("csurface create failed");
+        return false;
+    }
+
+    auto producer = cSurface_->GetProducer();
+    pSurface_ = Surface::CreateSurfaceAsProducer(producer);
+    if (pSurface_ == nullptr) {
+        ROSEN_LOGE("csurface create failed");
+        return false;
+    }
+
+    listener_ = new BufferListener(*this);
+    SurfaceError ret = cSurface_->RegisterConsumerListener(listener_);
+    if (ret != SURFACE_ERROR_OK) {
+        ROSEN_LOGE("listener register failed");
+        return false;
+    }
+    return true;
+}
+
+void RSInterfacesTestUtils::OnVsync()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    ROSEN_LOGI("RSInterfacesTestUtils::OnVsync");
+    sptr<SurfaceBuffer> cbuffer = nullptr;
+    int32_t fence = -1;
+    int64_t timestamp = 0;
+    OHOS::Rect damage;
+    if (cSurface_ == nullptr) {
+        ROSEN_LOGE("cSurface_ is null");
+        return;
+    }
+    auto sret = cSurface_->AcquireBuffer(cbuffer, fence, timestamp, damage);
+    UniqueFd fenceFd(fence);
+    if (cbuffer == nullptr || sret != OHOS::SURFACE_ERROR_OK) {
+        ROSEN_LOGE("acquire buffer failed");
+        return;
+    }
+    bufferHandle_ = cbuffer->GetBufferHandle();
+    if (bufferHandle_ == nullptr) {
+        ROSEN_LOGE("get bufferHandle failed");
+        return;
+    }
+    if (defaultWidth_ == static_cast<uint32_t>(bufferHandle_->width) &&
+        defaultHeight_ == static_cast<uint32_t>(bufferHandle_->height)) {
+        successCount_++;
+        ROSEN_LOGI("compareWH is successful in onVsync: %d", successCount_);
+    } else {
+        failCount_++;
+    }
+    if (cbuffer != prevBuffer_) {
+        if (prevBuffer_ != nullptr) {
+            SurfaceError ret = cSurface_->ReleaseBuffer(prevBuffer_, -1);
+            if (ret != SURFACE_ERROR_OK) {
+                ROSEN_LOGE("buffer release failed");
+                return;
+            }
+        }
+        prevBuffer_ = cbuffer;
+    }
+}
+
+void RSInterfacesTestUtils::RootNodeInit(std::shared_ptr<RSUIDirector> rsUiDirector, int width, int height)
+{
+    std::cout << "rs app demo Init Rosen Backend!" << std::endl;
+    rootNode_ = RSRootNode::Create();
+    rootNode_->SetBounds(0, 0, width, height);
+    rootNode_->SetFrame(0, 0, width, height);
+    rootNode_->SetBackgroundColor(SK_ColorRED);
+    rsUiDirector->SetRoot(rootNode_->GetId());
+}
+
+sptr<Window> RSInterfacesTestUtils::CreateWindowByDisplayParam(DisplayId displayId, std::string name,
+    Rect rect)
+{
+    sptr<WindowOption> option = new WindowOption();
+    if (option == nullptr) {
+        return nullptr;
+    }
+    Rect displayRect = {rect.posX_, rect.posY_, rect.width_, rect.height_};
+    option->SetDisplayId(displayId);
+    option->SetWindowRect(displayRect);
+    option->SetWindowType(WindowType::APP_MAIN_WINDOW_BASE);
+    option->SetWindowMode(WindowMode::WINDOW_MODE_FLOATING);
+    option->SetWindowName(name);
+    sptr<Window> window = Window::Create(option->GetWindowName(), option);
+    return window;
+}
+} // namespace ROSEN
+} // namespace OHOS

rosen/modules/render_service_client/test/systemtest/rs_interfaces_test_utils.h
@@ -1,0 +1,80 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FRAMEWORKS_RS_TEST_ST_RS_INTERFACES_TEST_UTILS_H
+#define FRAMEWORKS_RS_TEST_ST_RS_INTERFACES_TEST_UTILS_H
+
+#include <securec.h>
+
+#include "display.h"
+#include "display_info.h"
+#include "display_manager.h"
+#include "display_property.h"
+#include "dm_common.h"
+#include "screen.h"
+#include "screen_manager.h"
+#include "ui/rs_display_node.h"
+#include "ui/rs_root_node.h"
+#include "ui/rs_surface_node.h"
+#include "ui/rs_ui_director.h"
+#include "unique_fd.h"
+#include "window.h"
+#include "window_manager_hilog.h"
+#include "window_option.h"
+#include "wm_common.h"
+
+namespace OHOS {
+namespace Rosen {
+class RSInterfacesTestUtils {
+public:
+    ~RSInterfacesTestUtils();
+    bool CreateSurface();
+    void RootNodeInit(std::shared_ptr<RSUIDirector> rsUiDirector, int width, int height);
+    sptr<Window> CreateWindowByDisplayParam(DisplayId displayId, std::string name,
+        Rect rect);
+    class BufferListener : public IBufferConsumerListener {
+    public:
+        explicit BufferListener(RSInterfacesTestUtils &rsInterfacesTestUtils): utils_(rsInterfacesTestUtils)
+        {
+        }
+        ~BufferListener() noexcept override = default;
+        void OnBufferAvailable() override
+        {
+            utils_.OnVsync();
+        }
+
+    private:
+        RSInterfacesTestUtils &utils_;
+    };
+    friend class BufferListener;
+    
+    void OnVsync();
+    uint32_t successCount_ = 0;
+    uint32_t failCount_ = 0;
+    uint32_t defaultWidth_ = 0;
+    uint32_t defaultHeight_ = 0;
+    sptr<IBufferConsumerListener> listener_ = nullptr;
+    sptr<Surface> cSurface_ = nullptr; // consumer surface
+    sptr<Surface> pSurface_ = nullptr; // producer surface
+    sptr<SurfaceBuffer> prevBuffer_ = nullptr;
+    BufferHandle *bufferHandle_ = nullptr;
+    std::shared_ptr<RSNode> rootNode_ = nullptr;
+
+private:
+    std::mutex mutex_;
+};
+} // namespace ROSEN
+} // namespace OHOS
+#endif // FRAMEWORKS_RS_TEST_ST_RS_INTERFACES_TEST_UTILS_H