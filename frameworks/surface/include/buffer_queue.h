clipsheep6/graphic_2d

fix browser crash
Signed-off-by: BruceXuXu <xufeilong2@huawei.com>
Change-Id: I4aa2be0178f70cc64722bfd5c8d28984c53d11af

frameworks/surface/include/buffer_queue.h
@@ -99,6 +99,7 @@ public:
     uint32_t GetDefaultUsage();
 
     GSError CleanCache();
+    GSError OnConsumerDied();
 
     uint64_t GetUniqueId() const;
 
@@ -138,6 +139,7 @@ private:
     GSError CheckRequestConfig(const BufferRequestConfig &config);
     GSError CheckFlushConfig(const BufferFlushConfig &config);
     void DumpCache(std::string &result);
+    void ClearLocked();
 
     int32_t defaultWidth = 0;
     int32_t defaultHeight = 0;

frameworks/surface/src/buffer_queue.cpp
@@ -66,7 +66,7 @@ BufferQueue::BufferQueue(const std::string &name, bool isShared)
 BufferQueue::~BufferQueue()
 {
     BLOGNI("dtor, Queue id: %{public}" PRIu64 "", uniqueId_);
-    CleanCache();
+    OnConsumerDied();
 }
 
 GSError BufferQueue::Init()
@@ -387,6 +387,10 @@ GSError BufferQueue::DoFlushBuffer(uint32_t sequence, const sptr<BufferExtraData
     ScopedBytrace func(__func__);
     ScopedBytrace bufferName(name_ + ":" + std::to_string(sequence));
     std::lock_guard<std::mutex> lockGuard(mutex_);
+    if (bufferQueueCache_.find(sequence) == bufferQueueCache_.end()) {
+        BLOGN_FAILURE_ID(sequence, "not found in cache");
+        return GSERROR_NO_ENTRY;
+    }
     if (bufferQueueCache_[sequence].isDeleting) {
         DeleteBufferInCache(sequence);
         BLOGN_SUCCESS_ID(sequence, "delete");
@@ -486,6 +490,10 @@ GSError BufferQueue::ReleaseBuffer(sptr<SurfaceBuffer> &buffer, const sptr<SyncF
     }
 
     std::lock_guard<std::mutex> lockGuard(mutex_);
+    if (bufferQueueCache_.find(sequence) == bufferQueueCache_.end()) {
+        BLOGN_FAILURE_ID(sequence, "not find in cache, Queue id: %{public}" PRIu64 "", uniqueId_);
+        return GSERROR_NO_ENTRY;
+    }
     bufferQueueCache_[sequence].state = BUFFER_STATE_RELEASED;
     bufferQueueCache_[sequence].fence = fence;
 
@@ -778,6 +786,27 @@ uint32_t BufferQueue::GetDefaultUsage()
     return defaultUsage;
 }
 
+void BufferQueue::ClearLocked()
+{
+    for (auto &[id, _] : bufferQueueCache_) {
+        if (onBufferDelete_ != nullptr) {
+            onBufferDelete_(id);
+        }
+    }
+    bufferQueueCache_.clear();
+    freeList_.clear();
+    dirtyList_.clear();
+    deletingList_.clear();
+}
+
+GSError BufferQueue::OnConsumerDied()
+{
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    ClearLocked();
+    waitReqCon_.notify_all();
+    return GSERROR_OK;
+}
+
 GSError BufferQueue::CleanCache()
 {
     std::lock_guard<std::mutex> lockGuard(mutex_);
@@ -788,15 +817,7 @@ GSError BufferQueue::CleanCache()
         ScopedBytrace bufferIPCSend("OnCleanCache");
         listenerClazz_->OnCleanCache();
     }
-    for (auto &[id, _] : bufferQueueCache_) {
-        if (onBufferDelete_ != nullptr) {
-            onBufferDelete_(id);
-        }
-    }
-    bufferQueueCache_.clear();
-    freeList_.clear();
-    dirtyList_.clear();
-    deletingList_.clear();
+    ClearLocked();
     waitReqCon_.notify_all();
     return GSERROR_OK;
 }

eclipse-oniro-mirrors/graphic_graphic_2d

frameworks/surface/include/buffer_queue.h
@@ -99,6 +99,7 @@ public:
     uint32_t GetDefaultUsage();
 
     GSError CleanCache();
+    GSError GoBackground();
 
     uint64_t GetUniqueId() const;
 
@@ -138,6 +139,7 @@ private:
     GSError CheckRequestConfig(const BufferRequestConfig &config);
     GSError CheckFlushConfig(const BufferFlushConfig &config);
     void DumpCache(std::string &result);
+    void ClearLocked();
 
     int32_t defaultWidth = 0;
     int32_t defaultHeight = 0;

frameworks/surface/src/buffer_queue.cpp
@@ -387,6 +387,10 @@ GSError BufferQueue::DoFlushBuffer(uint32_t sequence, const sptr<BufferExtraData
     ScopedBytrace func(__func__);
     ScopedBytrace bufferName(name_ + ":" + std::to_string(sequence));
     std::lock_guard<std::mutex> lockGuard(mutex_);
+    if (bufferQueueCache_.find(sequence) == bufferQueueCache_.end()) {
+        BLOGN_FAILURE_ID(sequence, "not found in cache");
+        return GSERROR_NO_ENTRY;
+    }
     if (bufferQueueCache_[sequence].isDeleting) {
         DeleteBufferInCache(sequence);
         BLOGN_SUCCESS_ID(sequence, "delete");
@@ -486,6 +490,10 @@ GSError BufferQueue::ReleaseBuffer(sptr<SurfaceBuffer> &buffer, const sptr<SyncF
     }
 
     std::lock_guard<std::mutex> lockGuard(mutex_);
+    if (bufferQueueCache_.find(sequence) == bufferQueueCache_.end()) {
+        BLOGN_FAILURE_ID(sequence, "not find in cache, Queue id: %{public}" PRIu64 "", uniqueId_);
+        return GSERROR_NO_ENTRY;
+    }
     bufferQueueCache_[sequence].state = BUFFER_STATE_RELEASED;
     bufferQueueCache_[sequence].fence = fence;
 
@@ -778,16 +786,8 @@ uint32_t BufferQueue::GetDefaultUsage()
     return defaultUsage;
 }
 
-GSError BufferQueue::CleanCache()
+void BufferQueue::ClearLocked()
 {
-    std::lock_guard<std::mutex> lockGuard(mutex_);
-    if (listener_ != nullptr) {
-        ScopedBytrace bufferIPCSend("OnCleanCache");
-        listener_->OnCleanCache();
-    } else if (listenerClazz_ != nullptr) {
-        ScopedBytrace bufferIPCSend("OnCleanCache");
-        listenerClazz_->OnCleanCache();
-    }
     for (auto &[id, _] : bufferQueueCache_) {
         if (onBufferDelete_ != nullptr) {
             onBufferDelete_(id);
@@ -797,6 +797,27 @@ GSError BufferQueue::CleanCache()
     freeList_.clear();
     dirtyList_.clear();
     deletingList_.clear();
+}
+
+GSError BufferQueue::GoBackground()
+{
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    if (listener_ != nullptr) {
+        ScopedBytrace bufferIPCSend("OnCleanCache");
+        listener_->OnCleanCache();
+    } else if (listenerClazz_ != nullptr) {
+        ScopedBytrace bufferIPCSend("OnCleanCache");
+        listenerClazz_->OnCleanCache();
+    }
+    ClearLocked();
+    waitReqCon_.notify_all();
+    return GSERROR_OK;
+}
+
+GSError BufferQueue::CleanCache()
+{
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    ClearLocked();
     waitReqCon_.notify_all();
     return GSERROR_OK;
 }