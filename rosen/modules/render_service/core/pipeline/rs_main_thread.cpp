clipsheep6/graphic_2d

collect dirty nodes and judge if cacheable node changes
Signed-off-by: shiyueeee <nieshiyue@huawei.com>
Change-Id: I8bb7102d520f77cfcc919a8f5ae16244d4b8aeda

rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -621,7 +621,7 @@ void RSMainThread::CollectInfoForHardwareComposer()
         // setDirty for surfaceView if last frame is hardware enabled
         for (auto& surfaceNode : hardwareEnabledNodes_) {
             if (surfaceNode->IsLastFrameHardwareEnabled()) {
-                surfaceNode->SetDirty();
+                surfaceNode->SetBufferContentDirty();
                 activeProcessPids_.emplace(ExtractPid(surfaceNode->GetId()));
             }
         }

rosen/modules/render_service/core/pipeline/rs_render_service_listener.cpp
@@ -90,7 +90,7 @@ void RSRenderServiceListener::OnGoBackground()
         node->ResetBufferAvailableCount();
         node->CleanCache();
         if (node->IsLastFrameHardwareEnabled()) {
-            node->SetDirty();
+            node->SetBufferContentDirty();
             RSMainThread::Instance()->AddActivePid(ExtractPid(node->GetId()));
         }
         node->ResetHardwareEnabledStates();

rosen/modules/render_service_base/include/pipeline/rs_render_node.h
@@ -46,6 +46,7 @@ public:
 
     ~RSRenderNode() override;
     bool IsDirty() const override;
+    bool IsContentDirty() const override;
 
     std::pair<bool, bool> Animate(int64_t timestamp) override;
     // PrepareCanvasRenderNode in UniRender
@@ -175,6 +176,26 @@ public:
     float GetShadowRectOffsetY() const
     {
         return shadowRectOffsetY_;
+    }
+    
+    void SetDrawingCacheType(RSDrawingCacheType cacheType)
+    {
+        drawingCacheType_ = cacheType;
+    }
+
+    RSDrawingCacheType GetDrawingCacheType() const
+    {
+        return drawingCacheType_;
+    }
+
+    void SetDrawingCacheChanged(bool cacheChanged)
+    {
+        isDrawingCacheChanged_ = cacheChanged;
+    }
+
+    bool GetDrawingCacheChanged() const
+    {
+        return isDrawingCacheChanged_;
     }
 
     // driven render ///////////////////////////////////
@@ -339,6 +360,9 @@ private:
     sk_sp<SkSurface> cacheCompletedSurface_ = nullptr;
     std::atomic<bool> isStaticCached_ = false;
     CacheType cacheType_ = CacheType::NONE;
+    // drawing group cache
+    RSDrawingCacheType drawingCacheType_ = RSDrawingCacheType::DISABLED;
+    bool isDrawingCacheChanged_ = false;
 
     bool isMainThreadNode_ = true;
     bool hasFilter_ = false;

eclipse-oniro-mirrors/graphic_graphic_2d

rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -621,7 +621,7 @@ void RSMainThread::CollectInfoForHardwareComposer()
         // setDirty for surfaceView if last frame is hardware enabled
         for (auto& surfaceNode : hardwareEnabledNodes_) {
             if (surfaceNode->IsLastFrameHardwareEnabled()) {
-                surfaceNode->SetDirty();
+                surfaceNode->SetContentDirty();
                 activeProcessPids_.emplace(ExtractPid(surfaceNode->GetId()));
             }
         }

rosen/modules/render_service/core/pipeline/rs_render_service_listener.cpp
@@ -90,7 +90,7 @@ void RSRenderServiceListener::OnGoBackground()
         node->ResetBufferAvailableCount();
         node->CleanCache();
         if (node->IsLastFrameHardwareEnabled()) {
-            node->SetDirty();
+            node->SetContentDirty();
             RSMainThread::Instance()->AddActivePid(ExtractPid(node->GetId()));
         }
         node->ResetHardwareEnabledStates();

rosen/modules/render_service_base/include/pipeline/rs_render_node.h
@@ -46,6 +46,7 @@ public:
 
     ~RSRenderNode() override;
     bool IsDirty() const override;
+    bool IsContentDirty() const override;
 
     std::pair<bool, bool> Animate(int64_t timestamp) override;
     // PrepareCanvasRenderNode in UniRender
@@ -175,6 +176,26 @@ public:
     float GetShadowRectOffsetY() const
     {
         return shadowRectOffsetY_;
+    }
+    
+    void SetDrawingCacheType(RSDrawingCacheType cacheType)
+    {
+        drawingCacheType_ = cacheType;
+    }
+
+    RSDrawingCacheType GetDrawingCacheType() const
+    {
+        return drawingCacheType_;
+    }
+
+    void SetDrawingCacheChanged(bool cacheChanged)
+    {
+        isDrawingCacheChanged_ = cacheChanged;
+    }
+
+    bool GetDrawingCacheChanged() const
+    {
+        return isDrawingCacheChanged_;
     }
 
     // driven render ///////////////////////////////////
@@ -339,6 +360,9 @@ private:
     sk_sp<SkSurface> cacheCompletedSurface_ = nullptr;
     std::atomic<bool> isStaticCached_ = false;
     CacheType cacheType_ = CacheType::NONE;
+    // drawing group cache
+    RSDrawingCacheType drawingCacheType_ = RSDrawingCacheType::DISABLED_CACHE;
+    bool isDrawingCacheChanged_ = false;
 
     bool isMainThreadNode_ = true;
     bool hasFilter_ = false;