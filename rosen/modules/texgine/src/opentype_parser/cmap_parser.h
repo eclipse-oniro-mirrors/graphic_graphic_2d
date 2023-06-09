clipsheep6/graphic_2d

修改魔鬼数字，变量名规范
Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

rosen/modules/texgine/src/opentype_parser/cmap_parser.h
@@ -27,7 +27,7 @@ namespace TextEngine {
 struct CmapSubtable;
 class CmapParser {
 public:
-    static constexpr int32_t InvalidGlyphId = Ranges::InvalidGlyphId;
+    static constexpr int32_t INVALIDGLYPHID = Ranges::INVALIDGLYPHID;
 
     /*
      * @brief Parse the cmap data

rosen/modules/texgine/src/opentype_parser/ranges.h
@@ -31,7 +31,7 @@ public:
         uint32_t end;
         int32_t gid;
     };
-    static constexpr int32_t InvalidGlyphId = -1;
+    static constexpr int32_t INVALIDGLYPHID = -1;
 
     /*
      * @brief Add a new range

rosen/modules/texgine/src/typeface.cpp
@@ -110,7 +110,7 @@ bool Typeface::Has(uint32_t ch)
         }
     }
 
-    return cmapParser_->GetGlyphId(ch) != CmapParser::InvalidGlyphId;
+    return cmapParser_->GetGlyphId(ch) != CmapParser::INVALIDGLYPHID;
 }
 } // namespace TextEngine
 } // namespace Rosen

rosen/modules/texgine/src/utils/logger.cpp
@@ -20,7 +20,7 @@
 #include <iostream>
 #include <sys/syscall.h>
 #include <unistd.h>
-#define GetTid() syscall(__NR_gettid)
+#define GETTID() syscall(__NR_gettid)
 
 #ifdef LOGGER_NO_COLOR
 #define IF_COLOR(x)
@@ -167,7 +167,7 @@ void Logger::AppendFileFuncLine(Logger &logger, enum LOG_PHASE phase)
 void Logger::AppendPidTid(Logger &logger, enum LOG_PHASE phase)
 {
     if (phase == LOG_PHASE::BEGIN) {
-        logger << getpid() << ":" << GetTid() << " ";
+        logger << getpid() << ":" << GETTID() << " ";
     }
 }
 
@@ -212,7 +212,7 @@ Logger::Logger(const std::string &file, const std::string &func, int line, enum 
     {
         std::lock_guard<std::mutex> lock(scopeMutex_);
         // The number of space if enable scope
-        Align(scope_ * 2);
+        Align(scope_ * 2);  // 2 means multiple
     }
 #endif
 }

eclipse-oniro-mirrors/graphic_graphic_2d

Signed-off-by: zhangsongwei <zhangsongwei2@huawei.com>
Change-Id: Ia20d163469c4c89c8373e78a01130aa13bda1fb7

rosen/modules/texgine/src/opentype_parser/cmap_parser.h
@@ -27,7 +27,7 @@ namespace TextEngine {
 struct CmapSubtable;
 class CmapParser {
 public:
-    static constexpr int32_t InvalidGlyphId = Ranges::InvalidGlyphId;
+    static constexpr int32_t INVALID_GLYPH_ID = Ranges::INVALID_GLYPH_ID;
 
     /*
      * @brief Parse the cmap data

rosen/modules/texgine/src/opentype_parser/ranges.h
@@ -31,7 +31,7 @@ public:
         uint32_t end;
         int32_t gid;
     };
-    static constexpr int32_t InvalidGlyphId = -1;
+    static constexpr int32_t INVALID_GLYPH_ID = -1;
 
     /*
      * @brief Add a new range

rosen/modules/texgine/src/typeface.cpp
@@ -110,7 +110,7 @@ bool Typeface::Has(uint32_t ch)
         }
     }
 
-    return cmapParser_->GetGlyphId(ch) != CmapParser::InvalidGlyphId;
+    return cmapParser_->GetGlyphId(ch) != CmapParser::INVALID_GLYPH_ID;
 }
 } // namespace TextEngine
 } // namespace Rosen

rosen/modules/texgine/src/utils/logger.cpp
@@ -20,7 +20,7 @@
 #include <iostream>
 #include <sys/syscall.h>
 #include <unistd.h>
-#define GetTid() syscall(__NR_gettid)
+#define GET_TID() syscall(__NR_gettid)
 
 #ifdef LOGGER_NO_COLOR
 #define IF_COLOR(x)
@@ -167,7 +167,7 @@ void Logger::AppendFileFuncLine(Logger &logger, enum LOG_PHASE phase)
 void Logger::AppendPidTid(Logger &logger, enum LOG_PHASE phase)
 {
     if (phase == LOG_PHASE::BEGIN) {
-        logger << getpid() << ":" << GetTid() << " ";
+        logger << getpid() << ":" << GET_TID() << " ";
     }
 }
 
@@ -212,7 +212,7 @@ Logger::Logger(const std::string &file, const std::string &func, int line, enum 
     {
         std::lock_guard<std::mutex> lock(scopeMutex_);
         // The number of space if enable scope
-        Align(scope_ * 2);
+        Align(scope_ * 2);  // 2 means multiple
     }
 #endif
 }