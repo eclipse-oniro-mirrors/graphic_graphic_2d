clipsheep6/graphic_2d

add colorSpace get
Signed-off-by: genghao <genghao13@huawei.com>

rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp
@@ -828,6 +828,42 @@ int32_t RSRenderServiceConnection::SetScreenHDRFormat(ScreenId id, int32_t modeI
     }
 }
 
+int32_t RSRenderServiceConnection::GetScreenSupportedColorSpaces(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces)
+{
+    auto renderType = RSUniRenderJudgement::GetUniRenderEnabledType();
+    if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
+        return RSHardwareThread::Instance().ScheduleTask(
+            [=, &colorSpaces]() { return screenManager_->GetScreenSupportedColorSpaces(id, colorSpaces); }).get();
+    } else {
+        return mainThread_->ScheduleTask(
+            [=, &colorSpaces]() { return screenManager_->GetScreenSupportedColorSpaces(id, colorSpaces); }).get();
+    }
+}
+
+int32_t RSRenderServiceConnection::GetScreenColorSpace(ScreenId id, CM_ColorSpaceType& colorSpace)
+{
+    auto renderType = RSUniRenderJudgement::GetUniRenderEnabledType();
+    if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
+        return RSHardwareThread::Instance().ScheduleTask(
+            [=, &colorSpace]() { return screenManager_->GetScreenColorSpace(id, colorSpace); }).get();
+    } else {
+        return mainThread_->ScheduleTask(
+            [=, &colorSpace]() { return screenManager_->GetScreenColorSpace(id, colorSpace); }).get();
+    }
+}
+
+int32_t RSRenderServiceConnection::SetScreenColorSpace(ScreenId id, CM_ColorSpaceType colorSpace)
+{
+    auto renderType = RSUniRenderJudgement::GetUniRenderEnabledType();
+    if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
+        return RSHardwareThread::Instance().ScheduleTask(
+            [=]() { return screenManager_->SetScreenColorSpace(id, colorSpace); }).get();
+    } else {
+        return mainThread_->ScheduleTask(
+            [=]() { return screenManager_->SetScreenColorSpace(id, colorSpace); }).get();
+    }
+}
+
 int32_t RSRenderServiceConnection::GetScreenType(ScreenId id, RSScreenType& screenType)
 {
     std::lock_guard<std::mutex> lock(mutex_);

rosen/modules/render_service/core/pipeline/rs_render_service_connection.h
@@ -169,6 +169,12 @@ private:
 
     int32_t SetScreenHDRFormat(ScreenId id, int32_t modeIdx) override;
 
+    int32_t GetScreenSupportedColorSpaces(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces) override;
+
+    int32_t GetScreenColorSpace(ScreenId id, CM_ColorSpaceType& colorSpace) override;
+
+    int32_t SetScreenColorSpace(ScreenId id, CM_ColorSpaceType colorSpace) override;
+
     int32_t GetScreenType(ScreenId id, RSScreenType& screenType) override;
 
 #ifndef USE_ROSEN_DRAWING

rosen/modules/render_service/core/screen_manager/rs_screen_manager.cpp
@@ -1268,6 +1268,33 @@ int32_t RSScreenManager::SetScreenHDRFormatLocked(ScreenId id, int32_t modeIdx)
     return screens_.at(id)->SetScreenHDRFormat(modeIdx);
 }
 
+int32_t RSScreenManager::GetScreenSupportedColorSpacesLocked(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces) const
+{
+    if (screens_.count(id) == 0) {
+        RS_LOGW("RSScreenManager %{public}s: There is no screen for id %{public}" PRIu64 ".", __func__, id);
+        return StatusCode::SCREEN_NOT_FOUND;
+    }
+    return screens_.at(id)->GetScreenSupportedColorSpaces(colorSpaces);
+}
+
+int32_t RSScreenManager::GetScreenColorSpaceLocked(ScreenId id, CM_ColorSpaceType& colorSpace) const
+{
+    if (screens_.count(id) == 0) {
+        RS_LOGW("RSScreenManager %{public}s: There is no screen for id %{public}" PRIu64 ".", __func__, id);
+        return StatusCode::SCREEN_NOT_FOUND;
+    }
+    return screens_.at(id)->GetScreenColorSpace(colorSpace);
+}
+
+int32_t RSScreenManager::SetScreenColorSpaceLocked(ScreenId id, CM_ColorSpaceType colorSpace)
+{
+    if (screens_.count(id) == 0) {
+        RS_LOGW("RSScreenManager %{public}s: There is no screen for id %{public}" PRIu64 ".", __func__, id);
+        return StatusCode::SCREEN_NOT_FOUND;
+    }
+    return screens_.at(id)->SetScreenColorSpace(colorSpace);
+}
+
 int32_t RSScreenManager::GetScreenSupportedColorGamuts(ScreenId id, std::vector<ScreenColorGamut>& mode) const
 {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -1356,6 +1383,24 @@ int32_t RSScreenManager::SetScreenHDRFormat(ScreenId id, int32_t modeIdx)
 {
     std::lock_guard<std::mutex> lock(mutex_);
     return SetScreenHDRFormatLocked(id, modeIdx);
+}
+
+int32_t RSScreenManager::GetScreenSupportedColorSpaces(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces) const
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    return GetScreenSupportedColorSpacesLocked(id, colorSpaces);
+}
+
+int32_t RSScreenManager::GetScreenColorSpace(ScreenId id, CM_ColorSpaceType& colorSpace) const
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    return GetScreenColorSpaceLocked(id, colorSpace);
+}
+
+int32_t RSScreenManager::SetScreenColorSpace(ScreenId id, CM_ColorSpaceType colorSpace)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    return SetScreenColorSpaceLocked(id, colorSpace);
 }
 
 } // namespace impl

rosen/modules/render_service/core/screen_manager/rs_screen_manager.h
@@ -60,6 +60,7 @@ struct ScreenInfo {
 
     GraphicPixelFormat pixelFormat = GraphicPixelFormat::GRAPHIC_PIXEL_FMT_RGBA_8888;
     ScreenHDRFormat hdrFormat = ScreenHDRFormat::NOT_SUPPORT_HDR;
+    
     uint32_t GetRotatedWidth() const
     {
         return (rotation == ScreenRotation::ROTATION_0 || rotation == ScreenRotation::ROTATION_180) ? width : height;
@@ -174,6 +175,12 @@ public:
     virtual int32_t GetScreenHDRFormat(ScreenId id, ScreenHDRFormat& hdrFormat) const = 0;
 
     virtual int32_t SetScreenHDRFormat(ScreenId id, int32_t modeIdx) = 0;
+
+    virtual int32_t GetScreenSupportedColorSpaces(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces) const = 0;
+
+    virtual int32_t GetScreenColorSpace(ScreenId id, CM_ColorSpaceType& colorSpace) const = 0;
+
+    virtual int32_t SetScreenColorSpace(ScreenId id, CM_ColorSpaceType colorSpace) = 0;
 
     virtual void HandlePostureData(const SensorEvent * const event) = 0;
 
@@ -303,6 +310,12 @@ public:
 
     int32_t SetScreenHDRFormat(ScreenId id, int32_t modeIdx) override;
 
+    int32_t GetScreenSupportedColorSpaces(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces) const override;
+
+    int32_t GetScreenColorSpace(ScreenId id, CM_ColorSpaceType& colorSpace) const override;
+
+    int32_t SetScreenColorSpace(ScreenId id, CM_ColorSpaceType colorSpace) override;
+
     void HandlePostureData(const SensorEvent * const event) override;
 
     ScreenId GetActiveScreenId() const override;
@@ -360,6 +373,9 @@ private:
     int32_t GetScreenSupportedHDRFormatsLocked(ScreenId id, std::vector<ScreenHDRFormat>& hdrFormats) const;
     int32_t GetScreenHDRFormatLocked(ScreenId id, ScreenHDRFormat& hdrFormat) const;
     int32_t SetScreenHDRFormatLocked(ScreenId id, int32_t modeIdx);
+    int32_t GetScreenSupportedColorSpacesLocked(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces) const;
+    int32_t GetScreenColorSpaceLocked(ScreenId id, CM_ColorSpaceType& colorSpace) const;
+    int32_t SetScreenColorSpaceLocked(ScreenId id, CM_ColorSpaceType colorSpace);
 
     void RegisterSensorCallback();
     void UnRegisterSensorCallback();

rosen/modules/render_service/core/transaction/rs_render_service_connection_stub.cpp
@@ -850,6 +850,52 @@ int RSRenderServiceConnectionStub::OnRemoteRequest(
             reply.WriteInt32(result);
             break;
         }
+        case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::GET_SCREEN_SUPPORTED_COLORSPACES): {
+            auto token = data.ReadInterfaceToken();
+            if (token != RSIRenderServiceConnection::GetDescriptor()) {
+                ret = ERR_INVALID_STATE;
+                break;
+            }
+            ScreenId id = data.ReadUint64();
+            std::vector<uint32_t> colorSpacesSend;
+            std::vector<CM_ColorSpaceType> colorSpaces;
+            int32_t result = GetScreenSupportedColorSpaces(id, colorSpaces);
+            reply.WriteInt32(result);
+            if (result != StatusCode::SUCCESS) {
+                break;
+            }
+            std::copy(colorSpaces.begin(), colorSpaces.end(), std::back_inserter(colorSpacesSend));
+            reply.WriteUInt32Vector(colorSpacesSend);
+            break;
+        }
+        case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::GET_SCREEN_COLORSPACE): {
+            auto token = data.ReadInterfaceToken();
+            if (token != RSIRenderServiceConnection::GetDescriptor()) {
+                ret = ERR_INVALID_STATE;
+                break;
+            }
+            ScreenId id = data.ReadUint64();
+            CM_ColorSpaceType colorSpace;
+            int32_t result = GetScreenColorSpace(id, colorSpace);
+            reply.WriteInt32(result);
+            if (result != StatusCode::SUCCESS) {
+                break;
+            }
+            reply.WriteUint32(colorSpace);
+            break;
+        }
+        case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::SET_SCREEN_COLORSPACE): {
+            auto token = data.ReadInterfaceToken();
+            if (token != RSIRenderServiceConnection::GetDescriptor()) {
+                ret = ERR_INVALID_STATE;
+                break;
+            }
+            ScreenId id = data.ReadUint64();
+            CM_ColorSpaceType colorSpace = static_cast<CM_ColorSpaceType>(data.ReadInt32());
+            int32_t result = SetScreenColorSpace(id, colorSpace);
+            reply.WriteInt32(result);
+            break;
+        }
         case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::GET_SCREEN_TYPE): {
             auto token = data.ReadInterfaceToken();
             if (token != RSIRenderServiceConnection::GetDescriptor()) {

rosen/modules/render_service_base/src/platform/ohos/rs_render_service_client.cpp
@@ -747,6 +747,33 @@ int32_t RSRenderServiceClient::SetScreenHDRFormat(ScreenId id, int32_t modeIdx)
     return renderService->SetScreenHDRFormat(id, modeIdx);
 }
 
+int32_t RSRenderServiceClient::GetScreenSupportedColorSpaces(ScreenId id, std::vector<CM_ColorSpaceType>& colorSpaces)
+{
+    auto renderService = RSRenderServiceConnectHub::GetRenderService();
+    if (renderService == nullptr) {
+        return RENDER_SERVICE_NULL;
+    }
+    return renderService->GetScreenSupportedColorSpaces(id, colorSpaces);
+}
+
+int32_t RSRenderServiceClient::GetScreenColorSpace(ScreenId id, CM_ColorSpaceType& colorSpace)
+{
+    auto renderService = RSRenderServiceConnectHub::GetRenderService();
+    if (renderService == nullptr) {
+        return RENDER_SERVICE_NULL;
+    }
+    return renderService->GetScreenColorSpace(id, colorSpace);
+}
+
+int32_t RSRenderServiceClient::SetScreenColorSpace(ScreenId id, CM_ColorSpaceType colorSpace)
+{
+    auto renderService = RSRenderServiceConnectHub::GetRenderService();
+    if (renderService == nullptr) {
+        return RENDER_SERVICE_NULL;
+    }
+    return renderService->SetScreenColorSpace(id, colorSpace);
+}
+
 int32_t RSRenderServiceClient::GetScreenType(ScreenId id, RSScreenType& screenType)
 {
     auto renderService = RSRenderServiceConnectHub::GetRenderService();

eclipse-oniro-mirrors/graphic_graphic_2d

rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp
@@ -828,6 +828,42 @@ int32_t RSRenderServiceConnection::SetScreenHDRFormat(ScreenId id, int32_t modeI
     }
 }
 
+int32_t RSRenderServiceConnection::GetScreenSupportedColorSpaces(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces)
+{
+    auto renderType = RSUniRenderJudgement::GetUniRenderEnabledType();
+    if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
+        return RSHardwareThread::Instance().ScheduleTask(
+            [=, &colorSpaces]() { return screenManager_->GetScreenSupportedColorSpaces(id, colorSpaces); }).get();
+    } else {
+        return mainThread_->ScheduleTask(
+            [=, &colorSpaces]() { return screenManager_->GetScreenSupportedColorSpaces(id, colorSpaces); }).get();
+    }
+}
+
+int32_t RSRenderServiceConnection::GetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType& colorSpace)
+{
+    auto renderType = RSUniRenderJudgement::GetUniRenderEnabledType();
+    if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
+        return RSHardwareThread::Instance().ScheduleTask(
+            [=, &colorSpace]() { return screenManager_->GetScreenColorSpace(id, colorSpace); }).get();
+    } else {
+        return mainThread_->ScheduleTask(
+            [=, &colorSpace]() { return screenManager_->GetScreenColorSpace(id, colorSpace); }).get();
+    }
+}
+
+int32_t RSRenderServiceConnection::SetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType colorSpace)
+{
+    auto renderType = RSUniRenderJudgement::GetUniRenderEnabledType();
+    if (renderType == UniRenderEnabledType::UNI_RENDER_ENABLED_FOR_ALL) {
+        return RSHardwareThread::Instance().ScheduleTask(
+            [=]() { return screenManager_->SetScreenColorSpace(id, colorSpace); }).get();
+    } else {
+        return mainThread_->ScheduleTask(
+            [=]() { return screenManager_->SetScreenColorSpace(id, colorSpace); }).get();
+    }
+}
+
 int32_t RSRenderServiceConnection::GetScreenType(ScreenId id, RSScreenType& screenType)
 {
     std::lock_guard<std::mutex> lock(mutex_);

rosen/modules/render_service/core/pipeline/rs_render_service_connection.h
@@ -169,6 +169,12 @@ private:
 
     int32_t SetScreenHDRFormat(ScreenId id, int32_t modeIdx) override;
 
+    int32_t GetScreenSupportedColorSpaces(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces) override;
+
+    int32_t GetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType& colorSpace) override;
+
+    int32_t SetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType colorSpace) override;
+
     int32_t GetScreenType(ScreenId id, RSScreenType& screenType) override;
 
 #ifndef USE_ROSEN_DRAWING

rosen/modules/render_service/core/screen_manager/rs_screen_manager.cpp
@@ -1268,6 +1268,33 @@ int32_t RSScreenManager::SetScreenHDRFormatLocked(ScreenId id, int32_t modeIdx)
     return screens_.at(id)->SetScreenHDRFormat(modeIdx);
 }
 
+int32_t RSScreenManager::GetScreenSupportedColorSpacesLocked(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces) const
+{
+    if (screens_.count(id) == 0) {
+        RS_LOGW("RSScreenManager %{public}s: There is no screen for id %{public}" PRIu64 ".", __func__, id);
+        return StatusCode::SCREEN_NOT_FOUND;
+    }
+    return screens_.at(id)->GetScreenSupportedColorSpaces(colorSpaces);
+}
+
+int32_t RSScreenManager::GetScreenColorSpaceLocked(ScreenId id, GraphicCM_ColorSpaceType& colorSpace) const
+{
+    if (screens_.count(id) == 0) {
+        RS_LOGW("RSScreenManager %{public}s: There is no screen for id %{public}" PRIu64 ".", __func__, id);
+        return StatusCode::SCREEN_NOT_FOUND;
+    }
+    return screens_.at(id)->GetScreenColorSpace(colorSpace);
+}
+
+int32_t RSScreenManager::SetScreenColorSpaceLocked(ScreenId id, GraphicCM_ColorSpaceType colorSpace)
+{
+    if (screens_.count(id) == 0) {
+        RS_LOGW("RSScreenManager %{public}s: There is no screen for id %{public}" PRIu64 ".", __func__, id);
+        return StatusCode::SCREEN_NOT_FOUND;
+    }
+    return screens_.at(id)->SetScreenColorSpace(colorSpace);
+}
+
 int32_t RSScreenManager::GetScreenSupportedColorGamuts(ScreenId id, std::vector<ScreenColorGamut>& mode) const
 {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -1356,6 +1383,24 @@ int32_t RSScreenManager::SetScreenHDRFormat(ScreenId id, int32_t modeIdx)
 {
     std::lock_guard<std::mutex> lock(mutex_);
     return SetScreenHDRFormatLocked(id, modeIdx);
+}
+
+int32_t RSScreenManager::GetScreenSupportedColorSpaces(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces) const
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    return GetScreenSupportedColorSpacesLocked(id, colorSpaces);
+}
+
+int32_t RSScreenManager::GetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType& colorSpace) const
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    return GetScreenColorSpaceLocked(id, colorSpace);
+}
+
+int32_t RSScreenManager::SetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType colorSpace)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    return SetScreenColorSpaceLocked(id, colorSpace);
 }
 
 } // namespace impl

rosen/modules/render_service/core/screen_manager/rs_screen_manager.h
@@ -33,6 +33,7 @@
 #include <screen_manager/screen_types.h>
 #include <screen_manager/rs_virtual_screen_resolution.h>
 #include <surface.h>
+#include <surface_type.h>
 #include "sensor_agent.h"
 #include "sensor_agent_type.h"
 
@@ -60,6 +61,7 @@ struct ScreenInfo {
 
     GraphicPixelFormat pixelFormat = GraphicPixelFormat::GRAPHIC_PIXEL_FMT_RGBA_8888;
     ScreenHDRFormat hdrFormat = ScreenHDRFormat::NOT_SUPPORT_HDR;
+    
     uint32_t GetRotatedWidth() const
     {
         return (rotation == ScreenRotation::ROTATION_0 || rotation == ScreenRotation::ROTATION_180) ? width : height;
@@ -174,6 +176,12 @@ public:
     virtual int32_t GetScreenHDRFormat(ScreenId id, ScreenHDRFormat& hdrFormat) const = 0;
 
     virtual int32_t SetScreenHDRFormat(ScreenId id, int32_t modeIdx) = 0;
+
+    virtual int32_t GetScreenSupportedColorSpaces(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces) const = 0;
+
+    virtual int32_t GetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType& colorSpace) const = 0;
+
+    virtual int32_t SetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType colorSpace) = 0;
 
     virtual void HandlePostureData(const SensorEvent * const event) = 0;
 
@@ -303,6 +311,12 @@ public:
 
     int32_t SetScreenHDRFormat(ScreenId id, int32_t modeIdx) override;
 
+    int32_t GetScreenSupportedColorSpaces(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces) const override;
+
+    int32_t GetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType& colorSpace) const override;
+
+    int32_t SetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType colorSpace) override;
+
     void HandlePostureData(const SensorEvent * const event) override;
 
     ScreenId GetActiveScreenId() const override;
@@ -360,6 +374,9 @@ private:
     int32_t GetScreenSupportedHDRFormatsLocked(ScreenId id, std::vector<ScreenHDRFormat>& hdrFormats) const;
     int32_t GetScreenHDRFormatLocked(ScreenId id, ScreenHDRFormat& hdrFormat) const;
     int32_t SetScreenHDRFormatLocked(ScreenId id, int32_t modeIdx);
+    int32_t GetScreenSupportedColorSpacesLocked(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces) const;
+    int32_t GetScreenColorSpaceLocked(ScreenId id, GraphicCM_ColorSpaceType& colorSpace) const;
+    int32_t SetScreenColorSpaceLocked(ScreenId id, GraphicCM_ColorSpaceType colorSpace);
 
     void RegisterSensorCallback();
     void UnRegisterSensorCallback();

rosen/modules/render_service/core/transaction/rs_render_service_connection_stub.cpp
@@ -850,6 +850,52 @@ int RSRenderServiceConnectionStub::OnRemoteRequest(
             reply.WriteInt32(result);
             break;
         }
+        case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::GET_SCREEN_SUPPORTED_COLORSPACES): {
+            auto token = data.ReadInterfaceToken();
+            if (token != RSIRenderServiceConnection::GetDescriptor()) {
+                ret = ERR_INVALID_STATE;
+                break;
+            }
+            ScreenId id = data.ReadUint64();
+            std::vector<uint32_t> colorSpacesSend;
+            std::vector<GraphicCM_ColorSpaceType> colorSpaces;
+            int32_t result = GetScreenSupportedColorSpaces(id, colorSpaces);
+            reply.WriteInt32(result);
+            if (result != StatusCode::SUCCESS) {
+                break;
+            }
+            std::copy(colorSpaces.begin(), colorSpaces.end(), std::back_inserter(colorSpacesSend));
+            reply.WriteUInt32Vector(colorSpacesSend);
+            break;
+        }
+        case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::GET_SCREEN_COLORSPACE): {
+            auto token = data.ReadInterfaceToken();
+            if (token != RSIRenderServiceConnection::GetDescriptor()) {
+                ret = ERR_INVALID_STATE;
+                break;
+            }
+            ScreenId id = data.ReadUint64();
+            GraphicCM_ColorSpaceType colorSpace;
+            int32_t result = GetScreenColorSpace(id, colorSpace);
+            reply.WriteInt32(result);
+            if (result != StatusCode::SUCCESS) {
+                break;
+            }
+            reply.WriteUint32(colorSpace);
+            break;
+        }
+        case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::SET_SCREEN_COLORSPACE): {
+            auto token = data.ReadInterfaceToken();
+            if (token != RSIRenderServiceConnection::GetDescriptor()) {
+                ret = ERR_INVALID_STATE;
+                break;
+            }
+            ScreenId id = data.ReadUint64();
+            GraphicCM_ColorSpaceType colorSpace = static_cast<GraphicCM_ColorSpaceType>(data.ReadInt32());
+            int32_t result = SetScreenColorSpace(id, colorSpace);
+            reply.WriteInt32(result);
+            break;
+        }
         case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::GET_SCREEN_TYPE): {
             auto token = data.ReadInterfaceToken();
             if (token != RSIRenderServiceConnection::GetDescriptor()) {

rosen/modules/render_service_base/src/platform/ohos/rs_render_service_client.cpp
@@ -747,6 +747,33 @@ int32_t RSRenderServiceClient::SetScreenHDRFormat(ScreenId id, int32_t modeIdx)
     return renderService->SetScreenHDRFormat(id, modeIdx);
 }
 
+int32_t RSRenderServiceClient::GetScreenSupportedColorSpaces(ScreenId id, std::vector<GraphicCM_ColorSpaceType>& colorSpaces)
+{
+    auto renderService = RSRenderServiceConnectHub::GetRenderService();
+    if (renderService == nullptr) {
+        return RENDER_SERVICE_NULL;
+    }
+    return renderService->GetScreenSupportedColorSpaces(id, colorSpaces);
+}
+
+int32_t RSRenderServiceClient::GetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType& colorSpace)
+{
+    auto renderService = RSRenderServiceConnectHub::GetRenderService();
+    if (renderService == nullptr) {
+        return RENDER_SERVICE_NULL;
+    }
+    return renderService->GetScreenColorSpace(id, colorSpace);
+}
+
+int32_t RSRenderServiceClient::SetScreenColorSpace(ScreenId id, GraphicCM_ColorSpaceType colorSpace)
+{
+    auto renderService = RSRenderServiceConnectHub::GetRenderService();
+    if (renderService == nullptr) {
+        return RENDER_SERVICE_NULL;
+    }
+    return renderService->SetScreenColorSpace(id, colorSpace);
+}
+
 int32_t RSRenderServiceClient::GetScreenType(ScreenId id, RSScreenType& screenType)
 {
     auto renderService = RSRenderServiceConnectHub::GetRenderService();