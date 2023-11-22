clipsheep6/graphic_2d

fix compile problem while open drawing
Signed-off-by: Hengming Wang <wanghengming@huawei.com>

rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -1862,6 +1862,7 @@ void RSUniRenderVisitor::ProcessParallelDisplayRenderNode(RSDisplayRenderNode& n
 #endif
 }
 
+#ifndef USE_ROSEN_DRAWING
 sk_sp<SkImage> RSUniRenderVisitor::GetCacheImageFromMirrorNode(std::shared_ptr<RSDisplayRenderNode> mirrorNode)
 {
     sk_sp<SkImage> image = nullptr;
@@ -1879,6 +1880,28 @@ sk_sp<SkImage> RSUniRenderVisitor::GetCacheImageFromMirrorNode(std::shared_ptr<R
     }
     return image;
 }
+#else
+std::shared_ptr<Drawing::Image> RSUniRenderVisitor::GetCacheImageFromMirrorNode(
+    std::shared_ptr<RSDisplayRenderNode> mirrorNode)
+{
+    std::shared_ptr<Drawing::Image> image = nullptr;
+    auto cacheImage = mirrorNode->GetCacheImgForCapture();
+    if (cacheImage != nullptr) {
+        auto renderContext = renderEngine_->GetRenderContext();
+        if (renderContext != nullptr) {
+            auto grContext = renderContext->GetDrGPUContext();
+            auto imageBackendTexure = cacheImage->GetBackendTexture(false, nullptr);
+            if (grContext != nullptr && imageBackendTexure.IsValid()) {
+                Drawing::BitmapFormat bitmapFormat = {Drawing::ColorType::COLORTYPE_RGBA_8888,
+                    Drawing::AlphaType::ALPHATYPE_PERMUL};
+                image->BuildFromTexture(*grContext, imageBackendTexure.GetTextureInfo(),
+                    Drawing::TextureOrigin::BOTTOM_LEFT, bitmapFormat, nullptr);
+            }
+        }
+    }
+    return image;
+}
+#endif
 
 void RSUniRenderVisitor::ProcessDisplayRenderNode(RSDisplayRenderNode& node)
 {
@@ -2014,7 +2037,7 @@ void RSUniRenderVisitor::ProcessDisplayRenderNode(RSDisplayRenderNode& node)
                 canvas_->restoreToCount(saveCount);
             }
 #else
-            sk_sp<SkImage> newImage = GetCacheImageFromMirrorNode(mirrorNode);
+            std::shared_ptr<Drawing::Image> newImage = GetCacheImageFromMirrorNode(mirrorNode);
             bool parallelComposition = RSMainThread::Instance()->GetParallelCompositionEnabled();
             if (parallelComposition && newImage && !displayHasSkipSurface_[mirrorNode->GetScreenId()] &&
                 !displayHasSecSurface_[mirrorNode->GetScreenId()]) {
@@ -4499,7 +4522,11 @@ void RSUniRenderVisitor::ScaleMirrorIfNeed(RSDisplayRenderNode& node)
     }
     // If the width and height not match the main screen, calculate the dstRect.
     if (mainWidth != boundsWidth || mainHeight != boundsHeight) {
+#ifndef USE_ROSEN_DRAWING
         canvas_->clear(SK_ColorBLACK);
+#else
+        canvas_->Clear(SK_ColorBLACK);
+#endif
         float mirrorScale = 1.0f; // 1 for init scale
         float startX = 0.0f;
         float startY = 0.0f;
@@ -4510,8 +4537,13 @@ void RSUniRenderVisitor::ScaleMirrorIfNeed(RSDisplayRenderNode& node)
             mirrorScale = boundsWidth / mainWidth;
             startY = (boundsHeight - (mirrorScale * mainHeight)) / 2; // 2 for calc Y
         }
+#ifndef USE_ROSEN_DRAWING
         canvas_->translate(startX, startY);
         canvas_->scale(mirrorScale, mirrorScale);
+#else
+        canvas_->Translate(startX, startY);
+        canvas_->Scale(mirrorScale, mirrorScale);
+#endif
     }
 }
 } // namespace Rosen

rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
@@ -42,6 +42,7 @@ namespace {
 constexpr PropertyId ANONYMOUS_MODIFIER_ID = 0;
 }
 
+#ifndef USE_ROSEN_DRAWING
 static inline SkBlendMode ConvertToSkBlendMode(int blendMode)
 {
     static const std::unordered_map<int, SkBlendMode> skBlendModeLUT = {
@@ -57,11 +58,32 @@ static inline SkBlendMode ConvertToSkBlendMode(int blendMode)
 
     return skBlendModeLUT.at(blendMode);
 }
+#else
+static inline Drawing::BlendMode ConvertToSkBlendMode(int blendMode)
+{
+    static const std::unordered_map<int, Drawing::BlendMode> blendModeLUT = {
+        {static_cast<int>(RSColorBlendModeType::DST_IN), Drawing::BlendMode::DST_IN},
+        {static_cast<int>(RSColorBlendModeType::SRC_IN), Drawing::BlendMode::SRC_IN}
+    };
+
+    auto iter = blendModeLUT.find(blendMode);
+    if (iter == blendModeLUT.end()) {
+        ROSEN_LOGE("The desired color_blend_mode is undefined, and the Drawing::BlendMode::SRC is used.");
+        return Drawing::BlendMode::SRC;
+    }
+
+    return blendModeLUT.at(blendMode);
+}
+#endif
 
 static inline void EnableColorBlendModeFilterLayer(RSPaintFilterCanvas& canvas, int& colorBlendMode)
 {
     if (colorBlendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
+#ifndef USE_ROSEN_DRAWING
         canvas.saveLayer(nullptr, nullptr);
+#else
+        canvas.SaveLayer({nullptr, nullptr});
+#endif
         canvas.SaveAlpha();
         canvas.SetAlpha(1.0f);
     }
@@ -70,11 +92,19 @@ static inline void EnableColorBlendModeFilterLayer(RSPaintFilterCanvas& canvas, 
 static inline void EnableColorBlendModeMaskLayer(RSPaintFilterCanvas& canvas, int& colorBlendMode)
 {
     if (colorBlendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
+#ifndef USE_ROSEN_DRAWING
         SkBlendMode skBlendMode = ConvertToSkBlendMode(colorBlendMode);
         SkPaint maskPaint;
         maskPaint.setBlendMode(skBlendMode);
         SkCanvas::SaveLayerRec maskLayerRec(nullptr, &maskPaint, nullptr, 0);
         canvas.saveLayer(maskLayerRec);
+#else
+        Drawing::BlendMode blendMode = ConvertToSkBlendMode(colorBlendMode);
+        Drawing::Brush brush;
+        brush.SetBlendMode(blendMode);
+        Drawing::SaveLayerOps maskLayerRec(nullptr, &brush, nullptr, 0);
+        canvas.SaveLayer(maskLayerRec);
+#endif
     }
 }
 
@@ -292,7 +322,11 @@ void RSCanvasRenderNode::ProcessAnimatePropertyAfterChildren(RSPaintFilterCanvas
     int colorBlendMode = GetRenderProperties().GetColorBlendMode();
     if (colorBlendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
         canvas.RestoreAlpha();
+#ifndef USE_ROSEN_DRAWING
         canvas.restore();
+#else
+        canvas.Restore();
+#endif
     }
     if (GetRenderProperties().IsLightUpEffectValid()) {
         RSPropertiesPainter::DrawLightUpEffect(GetRenderProperties(), canvas);

eclipse-oniro-mirrors/graphic_graphic_2d

rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -1884,6 +1884,7 @@ void RSUniRenderVisitor::ProcessParallelDisplayRenderNode(RSDisplayRenderNode& n
 #endif
 }
 
+#ifndef USE_ROSEN_DRAWING
 sk_sp<SkImage> RSUniRenderVisitor::GetCacheImageFromMirrorNode(std::shared_ptr<RSDisplayRenderNode> mirrorNode)
 {
     sk_sp<SkImage> image = nullptr;
@@ -1901,6 +1902,28 @@ sk_sp<SkImage> RSUniRenderVisitor::GetCacheImageFromMirrorNode(std::shared_ptr<R
     }
     return image;
 }
+#else
+std::shared_ptr<Drawing::Image> RSUniRenderVisitor::GetCacheImageFromMirrorNode(
+    std::shared_ptr<RSDisplayRenderNode> mirrorNode)
+{
+    auto image = std::make_shared<Drawing::Image>();
+    auto cacheImage = mirrorNode->GetCacheImgForCapture();
+    if (cacheImage != nullptr) {
+        auto renderContext = renderEngine_->GetRenderContext();
+        if (renderContext != nullptr) {
+            auto grContext = renderContext->GetDrGPUContext();
+            auto imageBackendTexure = cacheImage->GetBackendTexture(false, nullptr);
+            if (grContext != nullptr && imageBackendTexure.IsValid()) {
+                Drawing::BitmapFormat bitmapFormat = {Drawing::ColorType::COLORTYPE_RGBA_8888,
+                    Drawing::AlphaType::ALPHATYPE_PERMUL};
+                image->BuildFromTexture(*grContext, imageBackendTexure.GetTextureInfo(),
+                    Drawing::TextureOrigin::BOTTOM_LEFT, bitmapFormat, nullptr);
+            }
+        }
+    }
+    return image;
+}
+#endif
 
 void RSUniRenderVisitor::ProcessDisplayRenderNode(RSDisplayRenderNode& node)
 {
@@ -2052,7 +2075,7 @@ void RSUniRenderVisitor::ProcessDisplayRenderNode(RSDisplayRenderNode& node)
 #if defined RS_ENABLE_GL
             glFinish();
 #endif
-            sk_sp<SkImage> cacheImageProcessed = GetCacheImageFromMirrorNode(mirrorNode);
+            std::shared_ptr<Drawing::Image> cacheImageProcessed = GetCacheImageFromMirrorNode(mirrorNode);
             if (cacheImageProcessed && !displayHasSkipSurface_[mirrorNode->GetScreenId()] &&
                 !displayHasSecSurface_[mirrorNode->GetScreenId()]) {
                 ScaleMirrorIfNeed(node);
@@ -4540,7 +4563,11 @@ void RSUniRenderVisitor::ScaleMirrorIfNeed(RSDisplayRenderNode& node)
     float boundsHeight = node.GetRenderProperties().GetBoundsHeight();
     // If the width and height not match the main screen, calculate the dstRect.
     if (mainWidth != boundsWidth || mainHeight != boundsHeight) {
+#ifndef USE_ROSEN_DRAWING
         canvas_->clear(SK_ColorBLACK);
+#else
+        canvas_->Clear(SK_ColorBLACK);
+#endif
         float mirrorScale = 1.0f; // 1 for init scale
         float startX = 0.0f;
         float startY = 0.0f;
@@ -4551,8 +4578,13 @@ void RSUniRenderVisitor::ScaleMirrorIfNeed(RSDisplayRenderNode& node)
             mirrorScale = boundsWidth / mainWidth;
             startY = (boundsHeight - (mirrorScale * mainHeight)) / 2; // 2 for calc Y
         }
+#ifndef USE_ROSEN_DRAWING
         canvas_->translate(startX, startY);
         canvas_->scale(mirrorScale, mirrorScale);
+#else
+        canvas_->Translate(startX, startY);
+        canvas_->Scale(mirrorScale, mirrorScale);
+#endif
     }
 }
 

rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
@@ -42,6 +42,7 @@ namespace {
 constexpr PropertyId ANONYMOUS_MODIFIER_ID = 0;
 }
 
+#ifndef USE_ROSEN_DRAWING
 static inline SkBlendMode ConvertToSkBlendMode(int blendMode)
 {
     static const std::unordered_map<int, SkBlendMode> skBlendModeLUT = {
@@ -57,11 +58,32 @@ static inline SkBlendMode ConvertToSkBlendMode(int blendMode)
 
     return skBlendModeLUT.at(blendMode);
 }
+#else
+static inline Drawing::BlendMode ConvertToBlendMode(int blendMode)
+{
+    static const std::unordered_map<int, Drawing::BlendMode> blendModeLUT = {
+        {static_cast<int>(RSColorBlendModeType::DST_IN), Drawing::BlendMode::DST_IN},
+        {static_cast<int>(RSColorBlendModeType::SRC_IN), Drawing::BlendMode::SRC_IN}
+    };
+
+    auto iter = blendModeLUT.find(blendMode);
+    if (iter == blendModeLUT.end()) {
+        ROSEN_LOGE("The desired color_blend_mode is undefined, and the Drawing::BlendMode::SRC is used.");
+        return Drawing::BlendMode::SRC;
+    }
+
+    return blendModeLUT.at(blendMode);
+}
+#endif
 
 static inline void EnableColorBlendModeFilterLayer(RSPaintFilterCanvas& canvas, int& colorBlendMode)
 {
     if (colorBlendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
+#ifndef USE_ROSEN_DRAWING
         canvas.saveLayer(nullptr, nullptr);
+#else
+        canvas.SaveLayer({nullptr, nullptr});
+#endif
         canvas.SaveAlpha();
         canvas.SetAlpha(1.0f);
     }
@@ -70,11 +92,19 @@ static inline void EnableColorBlendModeFilterLayer(RSPaintFilterCanvas& canvas, 
 static inline void EnableColorBlendModeMaskLayer(RSPaintFilterCanvas& canvas, int& colorBlendMode)
 {
     if (colorBlendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
+#ifndef USE_ROSEN_DRAWING
         SkBlendMode skBlendMode = ConvertToSkBlendMode(colorBlendMode);
         SkPaint maskPaint;
         maskPaint.setBlendMode(skBlendMode);
         SkCanvas::SaveLayerRec maskLayerRec(nullptr, &maskPaint, nullptr, 0);
         canvas.saveLayer(maskLayerRec);
+#else
+        Drawing::BlendMode blendMode = ConvertToBlendMode(colorBlendMode);
+        Drawing::Brush brush;
+        brush.SetBlendMode(blendMode);
+        Drawing::SaveLayerOps maskLayerRec(nullptr, &brush, nullptr, 0);
+        canvas.SaveLayer(maskLayerRec);
+#endif
     }
 }
 
@@ -292,7 +322,11 @@ void RSCanvasRenderNode::ProcessAnimatePropertyAfterChildren(RSPaintFilterCanvas
     int colorBlendMode = GetRenderProperties().GetColorBlendMode();
     if (colorBlendMode != static_cast<int>(RSColorBlendModeType::NONE)) {
         canvas.RestoreAlpha();
+#ifndef USE_ROSEN_DRAWING
         canvas.restore();
+#else
+        canvas.Restore();
+#endif
     }
     if (GetRenderProperties().IsLightUpEffectValid()) {
         RSPropertiesPainter::DrawLightUpEffect(GetRenderProperties(), canvas);