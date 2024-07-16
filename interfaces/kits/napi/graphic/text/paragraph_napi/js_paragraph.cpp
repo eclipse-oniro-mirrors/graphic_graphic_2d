clipsheep6/graphic_2d

提供TS层字体、Run、Line的Metrics属性暴露接口
Signed-off-by: changleipeng <changleipeng4@huawei.com>
Change-Id: I764fa34b48f046df0245b991b8895ac992bebba4

interfaces/kits/napi/graphic/text/paragraph_napi/js_paragraph.cpp
@@ -82,6 +82,8 @@ napi_value JsParagraph::Init(napi_env env, napi_value exportObj)
         DECLARE_NAPI_FUNCTION("getTextLines", JsParagraph::GetTextLines),
         DECLARE_NAPI_FUNCTION("getActualTextRange", JsParagraph::GetActualTextRange),
         DECLARE_NAPI_FUNCTION("getLineMetrics", JsParagraph::GetLineMetrics),
+        DECLARE_NAPI_FUNCTION("getFontMetricsByTextStyle", JsParagraph::GetFontMetricsByTextStyle),
+        DECLARE_NAPI_FUNCTION("getLineFontMetrics", JsParagraph::GetLineFontMetrics),
     };
     napi_value constructor = nullptr;
     napi_status status = napi_define_class(env, CLASS_NAME.c_str(), NAPI_AUTO_LENGTH, Constructor, nullptr,
@@ -626,6 +628,77 @@ napi_value JsParagraph::OnGetLineMetricsAt(napi_env env, napi_callback_info info
         return nullptr;
     }
     return CreateLineMetricsJsValue(env, lineMetrics);
+}
+
+napi_value JsParagraph::GetFontMetricsByTextStyle(napi_env env, napi_callback_info info)
+{
+    JsParagraph* me = CheckParamsAndGetThis<JsParagraph>(env, info);
+    return (me != nullptr) ? me->OnGetFontMetricsByTextStyle(env, info) : nullptr;
+}
+
+napi_value JsParagraph::OnGetFontMetricsByTextStyle(napi_env env, napi_callback_info info)
+{
+    if (paragraph_ == nullptr) {
+        ROSEN_LOGE("JsParagraph::OnGetFontMetricsByTextStyle paragraph_ is nullptr");
+        return NapiThrowError(env, DrawingErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    size_t argc = ARGC_ONE;
+    napi_value argv[ARGC_ONE] = {nullptr};
+    napi_status status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (status != napi_ok || argc < ARGC_ONE) {
+        ROSEN_LOGE("JsParagraph::OnGetFontMetricsByTextStyle Argc is invalid: %{public}zu", argc);
+        return NapiThrowError(env, DrawingErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    TextStyle textStyle;
+    if (!GetTextStyleFromJS(env, argv[0], textStyle)) {
+        ROSEN_LOGE("JsParagraph::OnGetLineWidth Argv is invalid");
+        return NapiGetUndefined(env);
+    }
+
+    OHOS::Rosen::Drawing::FontMetrics fontmetrics = paragraph_->GetFontMetrics(textStyle);
+    return GetFontMetricsAndConvertToJsValue(env, &fontmetrics);
+}
+
+napi_value JsParagraph::GetLineFontMetrics(napi_env env, napi_callback_info info)
+{
+    JsParagraph* me = CheckParamsAndGetThis<JsParagraph>(env, info);
+    return (me != nullptr) ? me->OnGetLineFontMetrics(env, info) : nullptr;
+}
+
+napi_value JsParagraph::OnGetLineFontMetrics(napi_env env, napi_callback_info info)
+{
+    if (paragraph_ == nullptr) {
+        ROSEN_LOGE("JsParagraph::OnGetLineFontMetrics paragraph_ is nullptr");
+        return NapiThrowError(env, DrawingErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    size_t argc = ARGC_ONE;
+    napi_value argv[ARGC_ONE] = {nullptr};
+    napi_status status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (status != napi_ok || argc < ARGC_ONE) {
+        ROSEN_LOGE("JsParagraph::OnGetLineFontMetrics Argc is invalid: %{public}zu", argc);
+        return NapiThrowError(env, DrawingErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    int lineNumber = 0;
+    if (!(ConvertFromJsValue(env, argv[0], lineNumber))) {
+        ROSEN_LOGE("JsParagraph::OnGetLineFontMetrics Argv is invalid");
+        return NapiGetUndefined(env);
+    }
+
+    napi_value returnFontMetrics = nullptr;
+    NAPI_CALL(env, napi_create_array(env, &returnFontMetrics));
+    size_t fontMetricsSize = 0;
+    std::vector<Drawing::FontMetrics> grabFontMetrics;
+    if (!paragraph_->GetLineFontMetrics(lineNumber, fontMetricsSize, grabFontMetrics)) {
+        ROSEN_LOGE("JsParagraph::OnGetLineFontMetrics GetLineFontMetrics failed");
+        return returnFontMetrics;
+    }
+
+    int num = static_cast<int>(grabFontMetrics.size());
+    for (int index = 0; index < num; ++index) {
+        napi_value jsValue = GetFontMetricsAndConvertToJsValue(env, &grabFontMetrics[index]);
+        napi_set_element(env, returnFontMetrics, index, jsValue);
+    }
+    return returnFontMetrics;
 }
 
 JsParagraph::JsParagraph(std::shared_ptr<Typography> typography)

interfaces/kits/napi/graphic/text/strut_style_napi/js_strut_style.cpp
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "canvas_napi/js_canvas.h"
+#include "js_strut_style.h"
+#include "js_drawing_utils.h"
+#include "recording/recording_canvas.h"
+#include "run_napi/js_run.h"
+#include "utils/log.h"
+
+namespace OHOS::Rosen {
+thread_local napi_ref JsStrutStyleManager::constructor_ = nullptr;
+const std::string CLASS_NAME = "StrutStyle";
+
+void JsStrutStyleManager::Destructor(napi_env env, void *nativeObject, void *finalize)
+{
+    (void)finalize;
+    if (nativeObject != nullptr) {
+        JsStrutStyleManager *napi = reinterpret_cast<JsStrutStyleManager *>(nativeObject);
+        delete napi;
+    }
+}
+
+napi_value JsStrutStyleManager::Init(napi_env env, napi_value exportObj)
+{
+    napi_property_descriptor descriptors[] = {
+        DECLARE_NAPI_STATIC_FUNCTION("isStrutStyleEqual", IsStrutStyleEqual),
+    };
+
+    napi_status status = napi_define_properties(env, exportObj, sizeof(descriptors) / sizeof(napi_property_descriptor),
+        descriptors);
+    if (status != napi_ok) {
+        return nullptr;
+    }
+    return exportObj;
+}
+
+napi_value JsStrutStyleManager::IsStrutStyleEqual(napi_env env, napi_callback_info info)
+{
+    size_t argc = ARGC_TWO;
+    napi_value argv[ARGC_TWO] = {nullptr};
+    napi_status status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (status != napi_ok || argc < ARGC_TWO) {
+        ROSEN_LOGE("JsStrutStyleManager::IsStrutStyleEqual Argc is invalid: %{public}zu", argc);
+        return NapiThrowError(env, DrawingErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+
+    TypographyStyle styleFrom;
+    TypographyStyle styleTo;
+    SetStrutStyleFromJS(env, argv[0], styleFrom);
+    SetStrutStyleFromJS(env, argv[1], styleTo);
+
+    bool equal = (styleFrom == styleTo);
+    return CreateJsValue(env, equal);
+}
+
+} // namespace OHOS::Rosen

eclipse-oniro-mirrors/graphic_graphic_2d

interfaces/kits/napi/graphic/text/paragraph_napi/js_paragraph.cpp
@@ -82,6 +82,8 @@ napi_value JsParagraph::Init(napi_env env, napi_value exportObj)
         DECLARE_NAPI_FUNCTION("getTextLines", JsParagraph::GetTextLines),
         DECLARE_NAPI_FUNCTION("getActualTextRange", JsParagraph::GetActualTextRange),
         DECLARE_NAPI_FUNCTION("getLineMetrics", JsParagraph::GetLineMetrics),
+        DECLARE_NAPI_FUNCTION("getFontMetricsByTextStyle", JsParagraph::GetFontMetricsByTextStyle),
+        DECLARE_NAPI_FUNCTION("getLineFontMetrics", JsParagraph::GetLineFontMetrics),
     };
     napi_value constructor = nullptr;
     napi_status status = napi_define_class(env, CLASS_NAME.c_str(), NAPI_AUTO_LENGTH, Constructor, nullptr,
@@ -626,6 +628,77 @@ napi_value JsParagraph::OnGetLineMetricsAt(napi_env env, napi_callback_info info
         return nullptr;
     }
     return CreateLineMetricsJsValue(env, lineMetrics);
+}
+
+napi_value JsParagraph::GetFontMetricsByTextStyle(napi_env env, napi_callback_info info)
+{
+    JsParagraph* me = CheckParamsAndGetThis<JsParagraph>(env, info);
+    return (me != nullptr) ? me->OnGetFontMetricsByTextStyle(env, info) : nullptr;
+}
+
+napi_value JsParagraph::OnGetFontMetricsByTextStyle(napi_env env, napi_callback_info info)
+{
+    if (paragraph_ == nullptr) {
+        TEXT_LOGE("JsParagraph::OnGetFontMetricsByTextStyle paragraph_ is nullptr");
+        return NapiThrowError(env, TextErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    size_t argc = ARGC_ONE;
+    napi_value argv[ARGC_ONE] = {nullptr};
+    napi_status status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (status != napi_ok || argc < ARGC_ONE) {
+        TEXT_LOGE("JsParagraph::OnGetFontMetricsByTextStyle Argc is invalid: %{public}zu", argc);
+        return NapiThrowError(env, TextErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    TextStyle textStyle;
+    if (!GetTextStyleFromJS(env, argv[0], textStyle)) {
+        TEXT_LOGE("JsParagraph::OnGetLineWidth Argv is invalid");
+        return NapiGetUndefined(env);
+    }
+
+    OHOS::Rosen::Drawing::FontMetrics fontmetrics = paragraph_->GetFontMetrics(textStyle);
+    return GetFontMetricsAndConvertToJsValue(env, &fontmetrics);
+}
+
+napi_value JsParagraph::GetLineFontMetrics(napi_env env, napi_callback_info info)
+{
+    JsParagraph* me = CheckParamsAndGetThis<JsParagraph>(env, info);
+    return (me != nullptr) ? me->OnGetLineFontMetrics(env, info) : nullptr;
+}
+
+napi_value JsParagraph::OnGetLineFontMetrics(napi_env env, napi_callback_info info)
+{
+    if (paragraph_ == nullptr) {
+        TEXT_LOGE("JsParagraph::OnGetLineFontMetrics paragraph_ is nullptr");
+        return NapiThrowError(env, TextErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    size_t argc = ARGC_ONE;
+    napi_value argv[ARGC_ONE] = {nullptr};
+    napi_status status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (status != napi_ok || argc < ARGC_ONE) {
+        TEXT_LOGE("JsParagraph::OnGetLineFontMetrics Argc is invalid: %{public}zu", argc);
+        return NapiThrowError(env, TextErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+    int lineNumber = 0;
+    if (!(ConvertFromJsValue(env, argv[0], lineNumber))) {
+        TEXT_LOGE("JsParagraph::OnGetLineFontMetrics Argv is invalid");
+        return NapiGetUndefined(env);
+    }
+
+    napi_value returnFontMetrics = nullptr;
+    NAPI_CALL(env, napi_create_array(env, &returnFontMetrics));
+    size_t fontMetricsSize = 0;
+    std::vector<Drawing::FontMetrics> grabFontMetrics;
+    if (!paragraph_->GetLineFontMetrics(lineNumber, fontMetricsSize, grabFontMetrics)) {
+        TEXT_LOGE("JsParagraph::OnGetLineFontMetrics GetLineFontMetrics failed");
+        return returnFontMetrics;
+    }
+
+    int num = static_cast<int>(grabFontMetrics.size());
+    for (int index = 0; index < num; ++index) {
+        napi_value jsValue = GetFontMetricsAndConvertToJsValue(env, &grabFontMetrics[index]);
+        napi_set_element(env, returnFontMetrics, index, jsValue);
+    }
+    return returnFontMetrics;
 }
 
 JsParagraph::JsParagraph(std::shared_ptr<Typography> typography)

interfaces/kits/napi/graphic/text/strut_style_napi/js_strut_style.cpp
@@ -1,0 +1,65 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "js_strut_style.h"
+#include "log_wrapper.h"
+
+namespace OHOS::Rosen {
+thread_local napi_ref JsStrutStyleManager::constructor_ = nullptr;
+const std::string CLASS_NAME = "StrutStyle";
+
+void JsStrutStyleManager::Destructor(napi_env env, void *nativeObject, void *finalize)
+{
+    (void)finalize;
+    if (nativeObject != nullptr) {
+        JsStrutStyleManager *napi = reinterpret_cast<JsStrutStyleManager *>(nativeObject);
+        delete napi;
+    }
+}
+
+napi_value JsStrutStyleManager::Init(napi_env env, napi_value exportObj)
+{
+    napi_property_descriptor descriptors[] = {
+        DECLARE_NAPI_STATIC_FUNCTION("isStrutStyleEqual", IsStrutStyleEqual),
+    };
+
+    napi_status status = napi_define_properties(env, exportObj, sizeof(descriptors) / sizeof(napi_property_descriptor),
+        descriptors);
+    if (status != napi_ok) {
+        return nullptr;
+    }
+    return exportObj;
+}
+
+napi_value JsStrutStyleManager::IsStrutStyleEqual(napi_env env, napi_callback_info info)
+{
+    size_t argc = ARGC_TWO;
+    napi_value argv[ARGC_TWO] = {nullptr};
+    napi_status status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (status != napi_ok || argc < ARGC_TWO) {
+        TEXT_LOGE("JsStrutStyleManager::IsStrutStyleEqual Argc is invalid: %{public}zu", argc);
+        return NapiThrowError(env, TextErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+    }
+
+    TypographyStyle styleFrom;
+    TypographyStyle styleTo;
+    SetStrutStyleFromJS(env, argv[0], styleFrom);
+    SetStrutStyleFromJS(env, argv[1], styleTo);
+
+    bool equal = (styleFrom == styleTo);
+    return CreateJsValue(env, equal);
+}
+
+} // namespace OHOS::Rosen